0468625883f2b4b7b7beb941fceefdf10c1a829c
dillin92 <dillin970@gmail.com>
2021-05-18 16:27:20 -0400

    diplaying issues and warning if there is paginated data

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issuesContainerEl = document.querySelector("issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 getRepoIssues = function(repo) {
     var apiUrl ="https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -6,6 +7,11 @@ getRepoIssues = function(repo) {
         if (response.ok) {
         response.json().then(function(data) {
             displayIssues(data);
+
+            // check if api has paginated issues
+            if (response.headers.get("Link")) {
+                displayWarning(repo);
+            }
         });
     }else {
         alert("there was a problem with your request!");
@@ -22,7 +28,7 @@ var displayIssues = function(issues) {
         issuesContainerEl.textContent = "This repos has no open issues !";
         return;
     }
-    
+
     for (var i = 0; i < issues.length; i++) {
         //create a link element to take users to the issue on github
         var issueEl = document.createElement("a");
@@ -53,4 +59,17 @@ var displayIssues = function(issues) {
         //issue container
         issuesContainerEl.appendChild(issueEl);
     }
+};
+
+var displayWarning = function(repo) {
+    //add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See Mopre Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
 };
